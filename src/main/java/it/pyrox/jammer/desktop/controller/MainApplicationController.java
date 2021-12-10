9b14367e9977456a39d2d16da1d307396eb74370
Pyr0x1 <Pyr0x1@users.noreply.github.com>
2021-12-10 18:43:32 +0100

    Implemented simple hover/selection mechanism

--- src/main/java/it/pyrox/jammer/desktop/controller/MainApplicationController.java
+++ src/main/java/it/pyrox/jammer/desktop/controller/MainApplicationController.java
@@ -4,10 +4,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import it.pyrox.jammer.desktop.util.Constants;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 
@@ -25,6 +27,8 @@ public class MainApplicationController implements Initializable {
 	@FXML
 	private TilePane tilePane2;
 	
+	private static final PseudoClass PSEUDO_CLASS_CHECKED = PseudoClass.getPseudoClass("checked");
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {				
 		for (int i = 0; i < 2; i++) {
@@ -35,8 +39,16 @@ public class MainApplicationController implements Initializable {
 			for (int j = 0; j < Constants.NUM_BLOCK_ROWS; j++) {
 				for (int k = 0; k < Constants.NUM_BLOCK_COLS; k++) {				
 					Image image = new Image(getClass().getResourceAsStream("/block.png"), 32, 32, true, false);
-					ImageView imageViewTmp = new ImageView(image);															
-					tilePaneTmp.getChildren().add(imageViewTmp);					
+					ImageView imageViewTmp = new ImageView(image);
+					Pane tmpPane = new Pane();
+					tmpPane.getStyleClass().add("selected");
+					tmpPane.getChildren().add(imageViewTmp);
+					tmpPane.setOnMouseClicked(event -> {
+						Pane clickedPane = (Pane) event.getSource();
+						boolean selected = clickedPane.getPseudoClassStates().contains(PSEUDO_CLASS_CHECKED);
+						clickedPane.pseudoClassStateChanged(PSEUDO_CLASS_CHECKED, !selected);
+					});
+					tilePaneTmp.getChildren().add(tmpPane);					
 				}				
 			}
 		}
